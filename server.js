656a98d91a9ccc6165936b576119b7580f371ced
Julian <cusco2891@gmail.com>
2021-12-10 17:41:29 -0500

    firebase admin sdk

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const admin = require('firebase-admin');
+
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+var serviceAccount =
+{
+    "type": process.env.type,
+    "project_id": process.env.project_id,
+    "private_key_id": process.env.private_key_id,
+    "private_key": process.env.private_key.replace(/\\n/g, '\n'),
+    "client_email": process.env.client_email,
+    "client_id": process.env.client_id,
+    "auth_uri": process.env.auth_uri,
+    "token_uri": process.env.token_uri,
+    "auth_provider_x509_cert_url": process.env.auth_provider,
+    "client_x509_cert_url": process.env.client
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://cannabisstand-dfdcb-default-rtdb.firebaseio.com"
+});
+
+const database = admin.database();
 
 //app.use(express.static(process.cwd() + "public"));
 app.use(express.static("public"));
